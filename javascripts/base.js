50977620665d55a9e0d044e6ca513714bbe9fa0d
Dipesh Acharya <xtranophilist@gmail.com>
2014-09-23 12:55:57 +0545

    implemented history.js

--- javascripts/base.js
+++ javascripts/base.js
@@ -2,11 +2,12 @@ $(function(){
 
     var esc_listener = function(e){
         if(e.keyCode == 27)
-            open_home();
+            open_home(true);
     }
 
     var open_panel = function(panel_name){
         var section = $('.section#'+panel_name);
+        var title =  $(section.find('h1')[0]).text();
         //hide the section name
         $(section.find('h1')[0]).toggle();
         //show the section content/panel
@@ -15,6 +16,7 @@ $(function(){
         section.off('click');
         //add escape listener
         $(document).on('keyup', esc_listener);
+        History.pushState({state:title}, 'Awecode | ' +title, '?'+title.toLowerCase());
     }
 
     var section_click_listener= function(){
@@ -52,7 +54,7 @@ $(function(){
         });
     }
 
-    var open_home = function(){
+    var open_home = function(back){
         var index = $('.section.active').index();
         var position = positions[index];
         $('.section.active').css({top: position.top, left: position.left}).removeClass('active inactive');
@@ -65,11 +67,25 @@ $(function(){
       },500);
         //remove escape listener
         $(document).off('keyup');
+        if (back){
+            History.back();
+        }
+        //add section click handlers
+        $('.section').on('click', section_click_listener);
     }
 
     var init = function(){
+
+        // Bind to StateChange Event
+        History.Adapter.bind(window,'statechange',function(){ // Note: We are using statechange instead of popstate
+            var State = History.getState(); // Note: We are using History.getState() instead of event.state
+        });
+
         absolutify();
+        //add section click handlers
         $('.section').on('click', section_click_listener);
+
+
     }
 
     init();
