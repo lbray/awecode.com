025b1a9e577f41af3bba72ee4728504f62c98896
Dipesh Acharya <xtranophilist@gmail.com>
2014-09-23 12:21:10 +0545

    fixed event listeners + refactoring + toggling of header/panel in section

--- javascripts/base.js
+++ javascripts/base.js
@@ -1,44 +1,77 @@
 $(function(){
 
-  var positions = []
+    var esc_listener = function(e){
+        if(e.keyCode == 27)
+            open_home();
+    }
 
-  $('.content > .section').each(function(){
-    var $this = $(this);
-    var index = $this.index();
+    var open_panel = function(panel_name){
+        var section = $('.section#'+panel_name);
+        //hide the section name
+        $(section.find('h1')[0]).toggle();
+        //show the section content/panel
+        $(section.find('.panel')[0]).toggle();
+        //remove section click listener
+        section.off('click');
+        //add escape listener
+        $(document).on('keyup', esc_listener);
+    }
 
-    positions[index] = {
-      'top'     : $this.offset().top - 90,
-      'left'     : $this.offset().left,
-      position: "absolute"
+    var section_click_listener= function(){
+        var self = this;
+        $('.section').not(self).removeClass('active').addClass('inactive');
+        var offset = $(self).offset();
+        $(self).addClass('active').css({top: 0, left: 0})
+        setTimeout(function(){
+          $('.section').not(self).fadeOut(200);
+          open_panel($(self).attr('id'));
+      },500)
+
     };
-  });
 
-   $('.content > .section').each(function(){
-    var $this = $(this);
-    var index = $this.index();
-    $this.css(positions[index])
-  });
+    var absolutify = function(){
 
-   $(document).on('keyup', function(e){
-      if(e.keyCode == 27){
+        //global because open_home uses it
+        positions = []
+
+        $('.content > .section').each(function(){
+            var $this = $(this);
+            var index = $this.index();
+
+            positions[index] = {
+              'top'     : $this.offset().top - 90,
+              'left'     : $this.offset().left,
+              position: "absolute"
+          };
+      });
+
+        $('.content > .section').each(function(){
+            var $this = $(this);
+            var index = $this.index();
+            $this.css(positions[index])
+        });
+    }
+
+    var open_home = function(){
         var index = $('.section.active').index();
         var position = positions[index];
         $('.section.active').css({top: position.top, left: position.left}).removeClass('active inactive');
-        
+        //hide all panels
+        $('.section .panel').hide();
+        $('.section h1').show();
+
         setTimeout(function(){
           $('.section').removeClass('active inactive').fadeIn(200)
-        },500)
-      }
-   });
+      },500);
+        //remove escape listener
+        $(document).off('keyup');
+    }
 
-  $('.section').on('click', function(){
-    var self = this;
-    $('.section').not(self).removeClass('active').addClass('inactive');
-    var offset = $(self).offset();
-    $(self).addClass('active').css({top: 0, left: 0})
-    setTimeout(function(){
-      $('.section').not(self).fadeOut(200);
-    },500)
-  });
+    var init = function(){
+        absolutify();
+        $('.section').on('click', section_click_listener);
+    }
+
+    init();
 
 });
