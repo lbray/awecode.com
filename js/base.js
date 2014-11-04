7aeff44a414fb5e575094dca7d508cce49f12206
Dipesh Acharya <xtranophilist@gmail.com>
2014-11-04 17:25:57 +0545

    navbar scroll hacks

--- js/base.js
+++ js/base.js
@@ -6,14 +6,55 @@
 
 var $document = $(document);
 
-//if(!$(window).scrollTop()) { //abuse 0 == false :)
-//  alert("You are at the top of this window");
+
+function isElementInViewport(el) {
+    var rect = el.getBoundingClientRect();
+
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /*or $(window).height() */
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth) /*or $(window).width() */
+        );
+}
+
+//function callback() {
+//    console.log('hidden');
+//}
+//
+//
+//function fireIfElementVisible(el, callback) {
+//    return function () {
+//        if (!isElementInViewport(el)) {
+//            callback();
+//        }
+//    }
 //}
 
-$document.scroll(function () {
-    if ($document.scrollTop() >= 50) {
-        console.log($document.scrollTop());
+//var handler = fireIfElementVisible('nav', callback);
+
+
+//$(window).on('DOMContentLoaded load resize scroll', handler);
+
+var watch_scroll = function () {
+    var $nav = $('nav');
+    if ($document.scrollTop() != 0) {
+        // move navbar programmatically as we are making it relative
+        if ($nav.css('position') == 'fixed') {
+            $nav.css('margin-top', '-' + $document.scrollTop() + 'px');
+            $nav.css('position', 'relative');
+        }
+        if (!isElementInViewport($nav[0])) {
+            $nav.addClass('top');
+        }
+        if (isElementInViewport($('.brand')[0])) {
+            $nav.removeClass('top');
+        }
     } else {
-        console.log($document.scrollTop());
+        $nav.css('position', 'fixed');
     }
+}
+
+$document.scroll(function () {
+    watch_scroll();
 });
