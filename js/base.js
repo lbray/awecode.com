847e9f317d3ac5aab6ff4cdf18a28e1e6ec97f32
Dipesh Acharya <xtranophilist@gmail.com>
2014-11-04 19:36:01 +0545

    more fixes

--- js/base.js
+++ js/base.js
@@ -1,5 +1,13 @@
 var $document = $(document);
 
+var sl = function (o) {
+    if (window.console) {
+        console.log(o);
+    } else {
+        alert(o);
+    }
+}
+
 
 function isElementInViewport(el) {
     var rect = el.getBoundingClientRect();
@@ -16,20 +24,26 @@ function isElementInViewport(el) {
 //$(window).on('DOMContentLoaded load resize scroll', handler);
 
 var watch_scroll = function () {
+    //copying scroll top
+    var scroll_top = $document.scrollTop() + 0;
     var $nav = $('nav');
-    $nav.css('height', $('nav .inner')[0].getBoundingClientRect().height + 'px');
-    if ($document.scrollTop() != 0) {
+    var nav_height = $('nav .inner')[0].getBoundingClientRect().height + 'px';
+    $nav.css('height', nav_height);
+    if (scroll_top != 0) {
         // move navbar programmatically as we are making it relative
         if ($nav.css('position') == 'fixed') {
-            $nav.css('margin-top', '-' + $document.scrollTop() + 'px');
+            $nav.css('margin-top', '-' + scroll_top + 'px');
             $nav.css('position', 'relative');
         }
         if (!isElementInViewport($nav[0])) {
             $nav.addClass('top');
         }
-        // if the navbar can go back to its place and fit above works section, send it there
-        if ($('.works')[0].getBoundingClientRect().top >= $('nav')[0].getBoundingClientRect().height) {
+        // if the navbar is on top but can go back to its place and fit above works section, send it there
+        if ($('.works')[0].getBoundingClientRect().top >= $('nav')[0].getBoundingClientRect().height && $nav.hasClass('top')) {
             $nav.removeClass('top').css('margin', '0');
+        }
+        if (!isElementInViewport($nav[0])) {
+            $nav.css('position', 'fixed');
         }
     } else {
         $nav.css('position', 'fixed');
