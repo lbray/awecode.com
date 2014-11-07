6ff8ad574ad1377db6bbcbf94ccf8e3b7c9b42bf
Dipesh Acharya <xtranophilist@gmail.com>
2014-11-07 18:46:59 +0545

    hide overlay menu on link click

--- js/base.js
+++ js/base.js
@@ -63,6 +63,8 @@ var watch_scroll = function () {
 //smooth scrolling
 $(function () {
     $('a[href*=#]:not([href=#])').click(function () {
+        //hide overlay menu
+        $('#show-menu').attr('checked', false);
         if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
             var target = $(this.hash);
             target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
